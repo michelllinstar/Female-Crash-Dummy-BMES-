fba26f36fa987c320eece2ca5aa24c349f019eec
Shreeya S <sh.shreeya2@gmail.com>
2026-03-05 21:51:07 -0800

    tried fix footer but it's lwk tweaking still

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,16 +1,21 @@
+import ucla from "../assets/ucla.png";
+import bmes from "../assets/ucla_bmes.png";
+
 const Footer = () => {
     return (
         <footer className="website-footer">
-            <img src="/"></img> 
-            {/* spark campaign image */}
-            <p>Made with ♡ by UCLA BMES Design Team!</p>
-            {/* bmes image */}
-            {/* socials??? */}
+            <img src={ucla} alt="UCLA" className="footer-img" style={{ height: '200px', width: 'auto', maxHeight: '200px' }} />
 
-            <p><a href="/">About Us</a></p>
-            <p><a href="/">Simulator</a></p>
-            <p><a href="/">Real Data</a></p>
-            <p><a href="/">Info</a></p>
+            <p className="footer-text">Made with ♡ by UCLA BMES Design Team!</p>
+
+            <img src={bmes} alt="UCLA BMES" className="footer-img" style={{ height: '200px', width: 'auto', maxHeight: '200px' }} />
+
+            <div style={{display: 'flex', flexDirection: 'column', gap: '4px', alignItems: 'flex-end'}} className="footer-links">
+                <a href="/about_us">About Us</a>
+                <a href="/simulator">Simulator</a>
+                <a href="/real_data">Real Data</a>
+                <a href="/info">Info</a>
+            </div>
         </footer>
     );
 };
