c4b385973555e5038eefae4b7fdd4fafabf1324b
Shreeya S <sh.shreeya2@gmail.com>
2026-04-02 19:12:55 -0700

    beautiful

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,27 +1,21 @@
 import React from "react";
 import "./Home.css";
 import TextBox from './components/textBox.js';
-import Button from './components/button.js';
-import Label from './components/label.js';
 import femalecd from './assets/femalecd.png';
 
 
 const Home: React.FC = () => {
-    const ps_title = "Product Statement";
-    const ps_text = "Our platform leverages cutting-edge bioengineering tools to develop wearable sensors \
-that monitor physiological signals in real time, ensuring accurate and personalized health insights across diverse users.";
-
-  const impact_title = "Impact";
   const impact_text = "Our crash test dummy addresses this gap by accurately representing female body geometry \
 and biomechanics using low-cost, accessible materials. This makes inclusive testing more affordable and scalable, \
 allowing vehicle safety designs to better account for women’s injury risk without the high cost of traditional crash test models.";
+  const ps_text = "According to a University of Virginia study, seatbelt-wearing female drivers are 47% more likely to \
+  sustain severe injuries than belt-restrained males in comparable crashes. Current crash tests rely primarily on \
+  50th-percentile male and 5th-percentile female dummies. As a result, vehicles remain largely untested for the average \
+  woman’s body geometry, biomechanics, and injury risk. ";
 
-  const pd_title = "Product Description";
-  const pd_text = "Some text about the product.";
+  const pd_text = "Our platform leverages cutting-edge bioengineering tools to develop wearable sensors \
+that monitor physiological signals in real time, ensuring accurate and personalized health insights across diverse users.";
 
-  const image_info1 = "placeholder";
-  const image_info2 = "placeholder";
-  const image_info3 = "placeholder";
 
   return (
     <div className="main-layout"> 
@@ -43,7 +37,7 @@ allowing vehicle safety designs to better account for women’s injury risk with
                           style={{ width: '350px', height: '148px' }}
                     />
                   </div>
-            </div>
+              </div>
             </div>
           <div className="buttons-container">
       <button className="Simulate" onClick={() => {}}>Simulate</button>
@@ -51,6 +45,31 @@ allowing vehicle safety designs to better account for women’s injury risk with
           <button className="AboutUs" onClick={() => {}}>About Us</button>
           <button className="RealData" onClick={() => {}}>Real Data</button>
           <button className="Information" onClick={() => {}}>Information</button>
+        </div>
+        <div className="descriptions">
+          <div className="description-block">
+            <h1>Problem Statement</h1>
+            <TextBox
+              text={ps_text}
+              className="bottom-textboxes"
+            />
+          </div>
+
+          <div className="description-block">
+            <h1 style={{textAlign: "right"}}>Impact</h1>
+            <TextBox
+              text={impact_text}
+              className="bottom-textboxes"
+            />
+          </div>
+          
+          <div className="description-block">
+            <h1>Product Description</h1>
+            <TextBox
+              text={`dummy go crash. ${pd_text}`}
+              className="bottom-textboxes"
+            />
+          </div>
         </div>
       </div>
   
