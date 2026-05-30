50d240a6cf868e5d2c1350c7acfaac3b57dead66
Priya R <priya@Priyas-MacBook-Air.local>
2026-05-30 13:23:45 -0700

    Update AboutUs and add new team assets

--- frontend/src/AboutUs.tsx
+++ frontend/src/AboutUs.tsx
@@ -3,25 +3,38 @@ import TeamMember from './components/TeamMember.jsx';
 import './AboutUs.css';
 import Button from './components/button.js';
 import meow from './assets/kitty2.png';
+import ellie from './assets/ellie2.jpg';
+import ellen from './assets/ellen2.png';
+import kevin from './assets/kevin.png';
+import justine from './assets/justine.png';
+import will from './assets/will.png';
+import nikita from './assets/nikita2.png';
+import miriam from './assets/miriam.png';
+import madeleine from './assets/madeleine2.png .png'; 
+import andrew from './assets/andrew.png';
+import celine from './assets/celine.png';
+import michelle from './assets/michelle.jpg';
+import priya from './assets/priya2.png';
+import shreeya from './assets/shreeya.png';
 
 const AboutUs: React.FC = () => {
   const teamData = [
-    { name: 'Ellie Lin', year: '3rd Year', major: 'Bioengineering', imageUrl: meow },
-    { name: 'Ellen Zulkarnain', year: '3rd Year', major: 'Mechanical Engineering', imageUrl: meow },
+    { name: 'Ellie Lin', year: '3rd Year', major: 'Bioengineering', imageUrl: ellie },
+    { name: 'Ellen Zulkarnain', year: '3rd Year', major: 'Mechanical Engineering', imageUrl: ellen },
 
-    { name: 'Kevin Liu', year: '3rd Year', major: 'Electrical Engineering', imageUrl: meow },
-    { name: 'Justine Lin', year: '2nd Year', major: 'Bioengineering', imageUrl: meow },
-    { name: 'Will Zheng', year: '2nd Year', major: 'Bioengineering', imageUrl: meow },
-    { name: 'Nikita Senthil', year: '3rd Year', major: 'Computer Engineering', imageUrl: meow },
+    { name: 'Kevin Liu', year: '3rd Year', major: 'Electrical Engineering', imageUrl: kevin },
+    { name: 'Justine Lin', year: '2nd Year', major: 'Bioengineering', imageUrl: justine },
+    { name: 'Will Zheng', year: '2nd Year', major: 'Bioengineering', imageUrl: will },
+    { name: 'Nikita Senthil', year: '3rd Year', major: 'Computer Engineering', imageUrl: nikita },
 
-    { name: 'Miriam Pomerantev', year: '2nd Year', major: 'Mechanial Engineering', imageUrl: meow },
-    { name: 'Madeleine Tsoi', year: '3rd Year', major: 'Bioengineering', imageUrl: meow },
-    { name: 'Andrew Cardoza', year: '3rd Year', major: 'Mechanical Engineering', imageUrl: meow },
-    { name: 'Celine Oh', year: '4th Year', major: 'Mechanical Engineering', imageUrl: meow }, 
+    { name: 'Miriam Pomerantev', year: '2nd Year', major: 'Mechanial Engineering', imageUrl: miriam },
+    { name: 'Madeleine Tsoi', year: '3rd Year', major: 'Bioengineering', imageUrl: madeleine },
+    { name: 'Andrew Cardoza', year: '3rd Year', major: 'Mechanical Engineering', imageUrl: andrew },
+    { name: 'Celine Oh', year: '4th Year', major: 'Mechanical Engineering', imageUrl: celine }, 
 
-    { name: 'Michelle Lin', year: '2nd Year', major: 'Computer Science', imageUrl: meow },
-    { name: 'Priya Ravi', year: '3rd Year', major: 'Bioengineering', imageUrl: meow },
-    { name: 'Shreeya Sharma', year: '1st Year', major: 'Computer Science', imageUrl: meow }
+    { name: 'Michelle Lin', year: '2nd Year', major: 'Computer Science', imageUrl: michelle },
+    { name: 'Priya Ravi', year: '3rd Year', major: 'Bioengineering', imageUrl: priya },
+    { name: 'Shreeya Sharma', year: '1st Year', major: 'Computer Science', imageUrl: shreeya }
   ];
 
   const group1 = teamData.slice(0, 2);
